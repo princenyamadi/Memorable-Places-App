a0c6a892acef3c1dbee48e607a335ae6c8d0a578
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-03 18:13:51 +0000

    Passed data from mainActivity to mapsActivity

--- app/src/main/java/tech/peny/memorableplaces/MainActivity.java
+++ app/src/main/java/tech/peny/memorableplaces/MainActivity.java
@@ -2,6 +2,7 @@ package tech.peny.memorableplaces;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -32,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(MainActivity.this,Integer.toString(position),Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                intent.putExtra("placeNumber",position);
+
+                startActivity(intent);
 
             }
         });
--- app/src/main/java/tech/peny/memorableplaces/MapsActivity.java
+++ app/src/main/java/tech/peny/memorableplaces/MapsActivity.java
@@ -2,7 +2,9 @@ package tech.peny.memorableplaces;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -37,6 +39,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        Intent intent = getIntent();
+        Toast.makeText(this,Integer.toString( intent.getIntExtra("placeNumber",0)),Toast.LENGTH_SHORT).show();
+
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
